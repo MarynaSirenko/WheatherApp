a6ea54af3ad5ee7abcef143dde1ab841723b4de4
MarynaSirenko <marina_sirenko@outlook.com>
2019-06-24 14:31:43 +0300

    Create a weather app

---
+++ weather/views.py
@@ -0,0 +1,44 @@
+#from requests import request
+import requests
+from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
+
+def index(request):
+    appid = '19b818fecab72ccd0d8badaf71d418b5'
+
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+
+    if (request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    try:
+        for city in cities:
+            res = requests.get(url.format(city.name)).json()
+            city_info = {
+                'city': city.name,
+                'temp': int(res["main"]['temp']),
+                'icon': res['weather'][0]['icon']
+            }
+            all_cities.append(city_info)
+    except KeyError:
+        pass
+
+    context = {'all_info': all_cities, 'form': form}
+    return render(request, 'weather/index.html', context)
+
+
+
+
+
+
+
